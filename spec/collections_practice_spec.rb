659f30aaf2d0a24de2263247b2ec1e3294ebc98c
user <github email address>
2020-10-30 10:42:32 -0400

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,7 +32,7 @@ describe 'collections practice' do
 
   # Question 4
   describe '#swap_elements_from_to' do
-    it 'swap the second and third elements of an array' do
+    it 'swap the second and third elements of an array using index, array, and destination' do
       expect(swap_elements_from_to(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end 
